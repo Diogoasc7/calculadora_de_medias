cce4e523fc1dc50d6a8e662b27578ce7bed069e5
Diogoasc7 <diogodsc1456@gmail.com>
2024-05-09 20:33:23 -0300

    Adicionei as imagens

---
+++ script.js
@@ -0,0 +1,60 @@
+const form = document.getElementById('form-atividade')
+const imgAprovado = '<img src="./imagem/aprovado.png" alt="Emoji celebrando" />'
+const imgReprovado = '<img src="./imagem/reprovado.png" alt="Emoji decepcionado" />'
+const atividades = []
+const notas = []
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>'
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"))
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    adicionaLinha()
+    atualizaTabela()
+    atualizaMediaFinal()
+})
+
+function adicionaLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade')
+    const inputNotaAtividade = document.getElementById('nota-atividade')
+
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade ${inputNomeAtividade.value} já foi inserida`)
+    } else {
+        atividades.push(inputNomeAtividade.value)
+        notas.push(parseFloat(inputNotaAtividade.value))
+
+        let linha = '<tr>'
+        linha += `<td>${inputNomeAtividade.value}</td>`
+        linha += `<td>${inputNotaAtividade.value}</td>`
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`
+        linha += '</tr>'
+
+        const corpoTabela = document.querySelector('tbody')
+        corpoTabela.insertAdjacentHTML('beforeend', linha)
+    }
+
+    inputNomeAtividade.value = ''
+    inputNotaAtividade.value = ''
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal()
+
+    const resultadoMediaFinal = document.getElementById('media-final-resultado')
+    resultadoMediaFinal.innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
+
+    const valorMediaFinal = document.getElementById('media-final-valor')
+    valorMediaFinal.textContent = mediaFinal.toFixed(2)
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i]
+    }
+
+    return somaDasNotas / notas.length
+}
